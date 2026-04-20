88cbbdf2d92894bab9c69b1600dab7c339ace9bd
theprotos <vlcz@seznam.cz>
2026-04-20 13:12:17 +0200

    meta: reorg azure related

--- azure/case1/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "resource_group_name" {
-  default = "myresource"
-}
-
-variable "location" {
-  default = "West US"
-}
--- azure/case1/variables.tfvars
+++
@@ -1,2 +0,0 @@
-resource_group_name = "demo-from-tfvars"
-location = "West Europe"
